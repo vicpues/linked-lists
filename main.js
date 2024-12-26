9b3542d078a1f42146fcd1ace12672579243ca1e
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-26 20:43:12 +0100

    test: Add a bunch of tests

--- main.js
+++ main.js
@@ -8,8 +8,29 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
-console.log(list.toString());
+
+console.log(list.size); // 6
+console.log(list.toString()); // ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
+
 list.insertAt("crocodile", 1);
-console.log(list.toString());
+console.log(list.size); // 7
+console.log(list.toString()); // ( dog ) -> ( crocodile ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
+
 list.removeAt(1);
-console.log(list.toString());
+console.log(list.size); // 6
+console.log(list.toString()); // ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
+
+list.prepend("cow");
+console.log(list.size); // 7
+console.log(list.toString()); // ( cow ) -> ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
+
+list.size = 10;
+console.log(list.size); // 7
+
+console.log(list.at(1)); // dog
+
+console.log(list.pop()); // turtle
+console.log(list.toString()); // ( cow ) -> ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> null
+
+console.log(list.shift()); // cow
+console.log(list.toString()); // ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> null
